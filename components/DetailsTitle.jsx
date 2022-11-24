5347f69e3d8a7866ec075f39831fdf758cabe19e
Raqs0 <94296574+Raqs0@users.noreply.github.com>
2022-11-25 02:29:53 +0800

    Add files via upload

---
+++ components/DetailsTitle.jsx
@@ -0,0 +1,11 @@
+import styles from "./DetailsTitle.module.css";
+
+const DetailsTitle = ({ }) => {
+  return (
+    <div className={styles.container}>
+      <h3>Details</h3>
+    </div>
+  );
+};
+
+export default DetailsTitle;

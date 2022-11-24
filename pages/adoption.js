26ca4590d548de13430dceb92b8cdfdff5d53f12
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-11-24 21:28:30 +0800

    .

--- pages/adoption.js
+++ pages/adoption.js
@@ -2,13 +2,16 @@ import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar";
 import HomepageBottom from "../components/HomepageBottom";
 import AdoptionContent from "../components/AdoptionContent";
-
+import AdoptionContentBottom from "../components/AdoptionContentBottom";
+import Footer from "../components/Footer";
 
 function adoption() {
   return (
     <div>
       <Navbar />
       <AdoptionContent/>
+      <AdoptionContentBottom/>
+      <Footer/>
 
 
     </div>

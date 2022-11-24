1325065b73064fb078e0fbd48720207b47f10877
leilabocar <marialeilamae.bocar@gsfe.tupcavite.edu.ph>
2022-11-25 00:41:47 +0800

    Sponsor page

--- components/HomepageBottom.jsx
+++ components/HomepageBottom.jsx
@@ -1,13 +1,17 @@
+import Button from "./Button";
 import styles from "./HomepageBottom.module.css";
+import Link from "next/link";
+
+
 
 const HomepageBottom = () => {
       return (
             <div className={styles.container}>
                   <h3>Looking for unconditional love? Visit the shelter today!</h3>
             <div className={styles.buttons}>
-                  <button variant="primary">DONATE</button>
-                  <button variant="primary">SPONSOR</button>
-                  <button variant="primary">ADOPT</button>
+                  <Button>DONATE</Button>
+                  <Link href="/sponsor"><Button>SPONSOR</Button></Link>
+                  <Button>ADOPT</Button>
             </div>
       </div>
       );
---
+++ pages/sponsor.js
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+import Sponsor from "../components/SponsorContent";
+import Footer from "../components/Footer";
+import AdoptionContentBottom from "../components/AdoptionContentBottom";
+
+
+function sponsor() {
+  return (
+    <div>
+      <Navbar />
+      <Sponsor />
+      <AdoptionContentBottom/>
+      <Footer/>
+
+
+    </div>
+  );
+}
+
+export default  sponsor;

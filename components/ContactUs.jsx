f68b0e9e0131c83cb078246319cbbe627ce50fb4
Lyn Satsatin <91708115+LynSatsatin@users.noreply.github.com>
2022-11-25 01:48:16 +0800

    Update ContactUs.jsx

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -5,15 +5,17 @@ const ContactUs = () => {
             <div className={styles.container}>
                   <div className={styles.image}>
                         <img src="/images/image 1.png" />
+                        <h3>Contact Us</h3>
                   </div>
                   <div className={styles.label}>
-                        <h3>Contact Us</h3>
-                        
                         <p> If you have questions about donations or other ways you can help LABRATS: </p>
 
-                        <h2>Email:</h2> <p>info.labrats@gmail.com</p>
-                        <h2>Call:</h2> <p> 046 222 101 Monday to Friday, 9 am - 5 pm.</p>
-                        <h2>Head Office:</h2> <p>143 Padayon Street, Himlay, FL 69168 Imus, Cavite</p>
+                        <h2>Email:</h2> 
+                        <p>info.labrats@gmail.com</p>
+                        <h2>Call:</h2> 
+                        <p> 046 222 101 Monday to Friday, 9 am - 5 pm.</p>
+                        <h2>Head Office:</h2> 
+                        <p>143 Padayon Street, Himlay, FL 69168 Imus, Cavite</p>
                         
                   </div>
 

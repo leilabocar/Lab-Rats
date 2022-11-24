98eecad1859e57780810e0c3df737dd288f65719
Lyn Satsatin <91708115+LynSatsatin@users.noreply.github.com>
2022-11-25 00:03:16 +0800

    Add files via upload

---
+++ pages/Contact_Us.js
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar";
+import ContactUs from "../components/ContactUs";
+import AdoptionContentBottom from "../components/AdoptionContentBottom";
+import Footer from "../components/Footer";
+
+function ContactUs() {
+  return (
+    <div>
+    <Navbar />
+    <ContactUs/>
+    <Footer/>
+    <AdoptionContentBottom/>
+
+
+  </div>
+  );
+}
+
+export default ContactUs;

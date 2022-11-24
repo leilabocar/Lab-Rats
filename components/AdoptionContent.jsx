bf02869d99de454dd65dd86ea756a6199e907b11
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-11-24 20:46:53 +0800

    .

---
+++ components/AdoptionContent.jsx
@@ -0,0 +1,11 @@
+import styles from "./AdoptionContent.module.css";
+
+const AdoptionContent = ({ }) => {
+  return (
+    <div className={styles.container}>
+      <h3>Pets for Adoption</h3>
+    </div>
+  );
+};
+
+export default AdoptionContent;

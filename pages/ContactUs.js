6650e7c4cde6d6aa929ba8edcfba0b195fa46227
Lyn Satsatin <91708115+LynSatsatin@users.noreply.github.com>
2022-11-24 23:05:51 +0800

    Add files via upload

---
+++ pages/ContactUs.js
@@ -0,0 +1,18 @@
+import Hero from "../components/Hero/Hero";
+import Navbar from "../components/Navbar";
+import ContactUs from "../components/ContactUs";
+
+
+export default function ContactUs() {
+  return (
+    <div>
+    <Navbar />
+    <Hero/>
+    <ContactUs/>
+
+
+  </div>
+  );
+}
+
+export default ContactUs;

3941a4246c4fa8c8c4a11f1de930ecdf605cf75d
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-11-25 23:38:19 +0800

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
       return (
         <footer className={styles.container}>
-                  <h3>Copyright LabRats 2022</h3>
+                  <h3>Copyright © LabRats 2022</h3>
         </footer>
       );
     };

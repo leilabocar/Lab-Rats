65a84bc448e026ce1fa4ff3ffd9994cee54a9146
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-11-25 22:15:14 +0800

    .

--- components/HomepageBottom.jsx
+++ components/HomepageBottom.jsx
@@ -9,8 +9,13 @@ const HomepageBottom = () => {
             <div className={styles.container}>
                   <h3>Looking for unconditional love? Visit the shelter today!</h3>
             <div className={styles.buttons}>
+<<<<<<< Updated upstream
                   <Link href="/donate"><Button>DONATE</Button></Link>
                   <Link href="/sponsor"><Button>SPONSOR</Button></Link>
+=======
+                  <Button>DONATE</Button>
+                  <Button>SPONSOR</Button>
+>>>>>>> Stashed changes
                   <Button>ADOPT</Button>
             </div>
       </div>

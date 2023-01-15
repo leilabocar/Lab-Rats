a7ba6f078a1155349ed45b3381ab2e4e5c38b919
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2023-01-15 22:27:35 +0800

    .

---
+++ components/ForgotPass.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import styles from "./ForgotPass.module.css";
+import Link from "next/link";
+
+const ForgotPass = ({ }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+            <h1>Forgot Password?</h1>
+            <h6>No worries, we'll send you reset instructions.</h6>
+      </div>
+      <div>
+            <label className={styles.labels}>Email:</label>
+      </div>
+      <form className={styles.main_input_container}>
+            <div>
+                  <input type="text" placeholder="Enter your email"/>
+            </div>
+            <div className={styles.buttons}>
+                  <button className={styles.button} role="button">Reset Password</button>
+            </div>
+      </form>
+      <div className={styles.small}>
+            <Link href="/" className={styles.links}><h2>Back to Log In</h2></Link>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPass;
---
+++ pages/ForgotPassword.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import ForgotPass from "../components/ForgotPass";
+
+
+export default function forgotpassword() {
+  return (
+    <div>
+      <ForgotPass />
+
+  </div>
+  );
+}

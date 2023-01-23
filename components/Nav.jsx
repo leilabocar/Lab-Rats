d4e8db21812eda863a65c6b02b0c3f71a5ed1f40
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2023-01-23 22:05:00 +0800

    .

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,26 +7,41 @@ const Nav = ({ }) => {
       <div className={styles.container}>
             <div className={styles.navbar}>
                   <div className={styles.logo}>
-                        <p>LabRats</p>
+                        <p><Link href="/">LabRats</Link></p>
                   </div>
                   <div className={styles.items}>
                         <ul>
-                              <li>Login</li>
-                              <li>Sign Up</li>
+                              <li><Link href="/SignIn">Login</Link></li>
+                              <li><Link href="/SignUp">Sign Up</Link></li>
                         </ul>
                   </div>
             </div>
             <div className={styles.contents}>
                   <div className={styles.title}>
-                        <h1>Motion</h1>
-                        <h1>Detection</h1>
+                        <h1>Motion Detection</h1>
                   </div>
                   <div className={styles.text}>
                         <div className={styles.text1}>
-                              <p>a process of detecting a change in the position of an object relative to its surroundings or a change in the surroundings relative to an object.</p>
+                              <p><i>a process of detecting a change in the position of an object relative to its surroundings or a change in the surroundings relative to an object.</i></p>
                         </div>
                   </div>
             </div>
+            <div className={styles.laptop}>
+                  <img src="/laptop.png"></img>
+            </div>
+            <footer className={styles.footer1}>
+                  <div className={styles.aboutLogo}>
+                        <img src="/about.png"></img>
+                  </div>
+                  <div className={styles.aboutContent}>
+                        <div className={styles.contentTitle}>
+                              <h3>About Us</h3>
+                        </div>
+                        <div className={styles.contentContent}>
+                              <p>We are fourth-year students at the Technological University of the Philippines-Cavite. We are taking CPET 17 - Embedded System. This web app project is part of our final requirement, which is to create a NextJs application.</p>
+                        </div>
+                  </div>
+            </footer>
       </div>
       
   );

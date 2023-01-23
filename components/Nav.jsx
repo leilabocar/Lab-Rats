ffcdefacf311c2818ea1f3558d1b2b7f91c17d31
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2023-01-23 11:52:36 +0800

    .

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,17 +4,31 @@ import Link from "next/link";
 
 const Nav = ({ }) => {
   return (
-      <div className={styles.navbar}>
-            <div className={styles.logo}>
-                  <p>LabRats</p>
+      <div className={styles.container}>
+            <div className={styles.navbar}>
+                  <div className={styles.logo}>
+                        <p>LabRats</p>
+                  </div>
+                  <div className={styles.items}>
+                        <ul>
+                              <li>Login</li>
+                              <li>Sign Up</li>
+                        </ul>
+                  </div>
             </div>
-            <div className={styles.items}>
-                  <ul>
-                        <li>Login</li>
-                        <li>Sign Up</li>
-                  </ul>
+            <div className={styles.contents}>
+                  <div className={styles.title}>
+                        <h1>Motion</h1>
+                        <h1>Detection</h1>
+                  </div>
+                  <div className={styles.text}>
+                        <div className={styles.text1}>
+                              <p>a process of detecting a change in the position of an object relative to its surroundings or a change in the surroundings relative to an object.</p>
+                        </div>
+                  </div>
             </div>
       </div>
+      
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import Login from "../components/Login";
+import Homepage from "../components/Homepage";
 import Nav from "../components/Nav";
 
 export default function Home() {
   return (
     <div>
       <Nav />
-
   </div>
   );
 }
